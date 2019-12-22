a57bd0127d69438fcd5ae0bcfad5c9e7d5606208
Zachary Sciscente <zsciscente@gmail.com>
2019-12-22 04:23:28 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,19 @@
-def hello_t
 
+
+def hello_t(names)
+ if block_given?
+    counter = 0
+  
+    while counter < names.length
+    yield names[counter]
+     counter = counter + 1
+    end  
+    names
+  else
+    puts "Hey! No block was given!"
+  end
 end
 
 # call your method here!
+
 
